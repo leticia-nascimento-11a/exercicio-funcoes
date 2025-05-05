91b399fcd0dbf4ab929b8959e6ad6e18a842d131
leticianasc2019 <leticianasc2019@gmail.com>
2025-05-05 13:17:52 +0000

    resolução do exercício combustível

---
+++ pages/combustivel/combustivel.js
@@ -0,0 +1,11 @@
+function gastoCombustivel(tempo, velocidade) {
+    return (tempo * velocidade) / 12;
+}
+
+let gasto1 = gastoCombustivel(10, 85);
+let gasto2 = gastoCombustivel(2, 92);
+let gasto3 = gastoCombustivel(22, 67);
+
+console.log(`GASTO 1 = ${gasto1.toFixed(3)}`);
+console.log(`GASTO 2 = ${gasto2.toFixed(3)}`);
+console.log(`GASTO 3 = ${gasto3.toFixed(3)}`);
